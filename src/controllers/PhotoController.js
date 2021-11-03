67a3a88bebd5dbc491f1bbdf9ed14496c3006cfb
Junior Paula <slucivaldo509@gmail.com>
2021-11-03 17:30:49 -0300

    filtering the uploaded files

--- src/controllers/PhotoController.js
+++ src/controllers/PhotoController.js
@@ -1,9 +1,23 @@
 /** criar class PhotoControler que responderá o router
  * com 'json'.
  */
+import multer from 'multer';
+
+import multerConfig from '../config/multerConfig';
+
+const upload = multer(multerConfig).single('foto');
+
 class PhotoControler {
   async store(req, res) {
-    res.json(req.file);
+    return upload(req, res, (error) => {
+      if (error) {
+        return res.status(400).json({
+          errors: [error.code],
+        });
+      }
+
+      return res.json(req.file);
+    });
   }
 }
 
--- src/routes/photoRoutes.js
+++ src/routes/photoRoutes.js
@@ -1,14 +1,10 @@
 /** importando o Router do express via destruct */
 import { Router } from 'express';
-import multer from 'multer';
 
 import photoController from '../controllers/PhotoController';
-import multerConfig from '../config/multerConfig';
-
-const upload = multer(multerConfig);
 
 const router = new Router();
 
-router.post('/', upload.single('foto'), photoController.store);
+router.post('/', photoController.store);
 
 export default router;
