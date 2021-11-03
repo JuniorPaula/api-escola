8b23f9ee9c3f94652af27add53fcad42bac72410
Junior Paula <slucivaldo509@gmail.com>
2021-11-03 11:33:02 -0300

    ending users CRUD

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -10,9 +10,79 @@ class UserControler {
   async store(req, res) {
     try {
       const newUser = await User.create(req.body);
-      res.json(newUser);
+      return res.json(newUser);
     } catch (e) {
-      res.status(400).json({ errors: e.errors.map((err) => err.message) });
+      return res.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+
+  /** index -> método responsável por listar todos os usuário */
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  /** show -> método responsável por listar um usuário
+     * com parametro id.
+     */
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      return res.json(user);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  /** update -> método responsável por atualizar um usuário */
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['ID não existe!'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe!'],
+        });
+      }
+      const userUpdated = await user.update(req.body);
+
+      return res.json(userUpdated);
+    } catch (e) {
+      return res.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+
+  /** delete -> método responsável por detelar um usuário */
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['ID não existe!'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe!'],
+        });
+      }
+      await user.destroy();
+
+      return res.json(user);
+    } catch (e) {
+      return res.status(400).json({ errors: e.errors.map((err) => err.message) });
     }
   }
 }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -11,4 +11,20 @@ const router = new Router();
  *  do método store do userControler */
 router.post('/', userController.store);
 
+/** rota responsável por listar todos os usuários */
+router.get('/', userController.index);
+
+/** rota responsável por listar um usuários
+ * -> recebe parametro id
+ */
+router.get('/:id', userController.show);
+
+/** rota responsável por atualizar um usuário
+ * -> recebe parametro id
+ */
+router.put('/:id', userController.update);
+
+/** rota responsável por deletar um usuário */
+router.delete('/:id', userController.delete);
+
 export default router;
