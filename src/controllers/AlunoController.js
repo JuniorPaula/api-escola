6d56f75f8025d95f287795801be76bec6409e9ad
Junior Paula <slucivaldo509@gmail.com>
2021-11-03 16:10:04 -0300

    created CRUD users

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -5,9 +5,109 @@
 import Aluno from '../models/Aluno';
 
 class AlunoControler {
+  /** método responsável por listar todos usuários */
   async index(req, res) {
     const alunos = await Aluno.findAll();
     res.json(alunos);
+  }
+
+  /** método responsável por criar um usuário */
+  async store(req, res) {
+    try {
+      const aluno = await Aluno.create(req.body);
+
+      return res.json(aluno);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  /** método responsável por listar um usuário com base no ID */
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['ID Requerido!'],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não existe!'],
+        });
+      }
+
+      return res.json(aluno);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  /** método responsável por atualizar um usuário com base no ID */
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['ID Requerido!'],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não existe!'],
+        });
+      }
+
+      const alunoUpdated = await aluno.update(req.body);
+
+      return res.json(alunoUpdated);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  /** método responsável por deletar um usuário com base no ID */
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['ID Requerido!'],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não existe!'],
+        });
+      }
+
+      await aluno.destroy();
+      return res.json({
+        apagado: true,
+      });
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
   }
 }
 
--- src/routes/alunoRoutes.js
+++ src/routes/alunoRoutes.js
@@ -3,8 +3,24 @@ import { Router } from 'express';
 
 import alunoController from '../controllers/AlunoController';
 
+/** importando o middleware de login */
+import loginRequired from '../middlewares/loginRequired';
+
 const router = new Router();
 
+/** index -> rota responsável por lista todos os usuários */
 router.get('/', alunoController.index);
+
+/** store -> rota responsável por criar usuários */
+router.post('/', loginRequired, alunoController.store);
+
+/** update -> rota responsável por atualizar um usuário com base no ID */
+router.put('/:id', loginRequired, alunoController.update);
+
+/** show -> rota responsável por mostrar um usuário com base no ID */
+router.get('/:id', alunoController.show);
+
+/** delete -> rota responsável por deletar um usuário com base no ID */
+router.delete('/:id', loginRequired, alunoController.delete);
 
 export default router;
